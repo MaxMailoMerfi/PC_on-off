35b2067668438874e6142b397fcc460948a843a7
Max <zenchmax@gmail.com>
2025-08-12 22:55:44 +0300

    HOME

--- .vscode/copy_bin.bat
+++ .vscode/copy_bin.bat
@@ -3,15 +3,20 @@ REM --- Перехід до кореня проєкту ---
 cd /d "%~dp0\.."
 
 REM --- Пошук BIN файлу у build ---
+set "SRC="
 for %%f in (build\*.bin) do (
     set "SRC=%%f"
-    set "DST=%%~nxf"
 )
 
 REM --- Перевірка і копіювання ---
 if defined SRC (
-    copy /Y "%SRC%" "%DST%"
-    echo The BIN file has been copied!
+    setlocal enabledelayedexpansion
+    set "DST=%~n1"
+    set "DST=!SRC:build\=!"
+    set "DST=!DST:.ino.bin=.bin!"
+    copy /Y "%SRC%" "!DST!"
+    echo The BIN file has been copied as "!DST!"!
+    endlocal
 ) else (
     echo BIN file not found!
 )
